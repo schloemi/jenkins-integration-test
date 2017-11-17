c724fe12568124edb0d1878465ea00e7688a9759
jschloemer <schloemi@schloemi.de>
2017-11-17 13:09:08 +0100

    Readded test

--- src/test/java/de/schloemi/jenkinstest/controllers/IndexControllerTest.java
+++ src/test/java/de/schloemi/jenkinstest/controllers/IndexControllerTest.java
@@ -14,7 +14,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class IndexControllerTest {
 
-/*
     IndexController controller;
 
     @Before
@@ -31,5 +30,5 @@ public class IndexControllerTest {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"));
-    }*/
+    }
 }
