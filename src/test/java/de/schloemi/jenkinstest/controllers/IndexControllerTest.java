c19cb36ef5d10e8fb82283735da9dffcd2284934
Jörg Schlömer <joerg.schloemer@acondix.de>
2017-11-17 10:52:40 +0100

    initial commit

---
+++ src/test/java/de/schloemi/jenkinstest/controllers/IndexControllerTest.java
@@ -0,0 +1,35 @@
+package de.schloemi.jenkinstest.controllers;
+
+import de.schloemi.jenkinstest.controller.IndexController;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+public class IndexControllerTest {
+
+
+    IndexController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new IndexController();
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
